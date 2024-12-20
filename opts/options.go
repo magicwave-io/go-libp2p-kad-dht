2c3e26a6d2252ccdf2c4c10673f383f75d35474f
a.gorulev <a.gorulev@kernelics.com>
2024-12-20 00:03:32 +0000

    magicwave.io

---
+++ opts/options.go
@@ -0,0 +1,135 @@
+package dhtopts
+
+import (
+	"fmt"
+	"time"
+
+	ds "github.com/ipfs/go-datastore"
+	dssync "github.com/ipfs/go-datastore/sync"
+	persist "github.com/libp2p/go-libp2p-kad-dht/persist"
+	pstore "github.com/libp2p/go-libp2p-peerstore"
+	"github.com/libp2p/go-libp2p-protocol"
+	record "github.com/libp2p/go-libp2p-record"
+)
+
+var ProtocolDHT protocol.ID = "/ipfs/kad/1.0.0"
+var ProtocolDHTOld protocol.ID = "/ipfs/dht"
+var DefaultProtocols = []protocol.ID{ProtocolDHT, ProtocolDHTOld}
+var DefaultSnapshotInterval = 5 * time.Minute
+
+// Options is a structure containing all the options that can be used when constructing a DHT.
+type Options struct {
+	Datastore        ds.Batching
+	Validator        record.Validator
+	Client           bool
+	Protocols        []protocol.ID
+	Snapshotter      persist.Snapshotter
+	Seeder           persist.Seeder
+	SnapshotInterval time.Duration
+	FallbackPeers    []pstore.PeerInfo
+}
+
+// Apply applies the given options to this Option
+func (o *Options) Apply(opts ...Option) error {
+	for i, opt := range opts {
+		if err := opt(o); err != nil {
+			return fmt.Errorf("dht option %d failed: %s", i, err)
+		}
+	}
+	return nil
+}
+
+// Option DHT option type.
+type Option func(*Options) error
+
+// Defaults are the default DHT options. This option will be automatically
+// prepended to any options you pass to the DHT constructor.
+var Defaults = func(o *Options) error {
+	o.Validator = record.NamespacedValidator{
+		"pk": record.PublicKeyValidator{},
+	}
+	o.Datastore = dssync.MutexWrap(ds.NewMapDatastore())
+	o.Protocols = DefaultProtocols
+	o.SnapshotInterval = DefaultSnapshotInterval
+	return nil
+}
+
+func Seeder(seeder persist.Seeder) Option {
+	return func(o *Options) error {
+		o.Seeder = seeder
+		return nil
+	}
+}
+
+func Snapshotter(snpshttr persist.Snapshotter, interval time.Duration) Option {
+	return func(o *Options) error {
+		o.Snapshotter = snpshttr
+		o.SnapshotInterval = interval
+		return nil
+	}
+}
+
+func FallbackPeers(fallback []pstore.PeerInfo) Option {
+	return func(o *Options) error {
+		o.FallbackPeers = fallback
+		return nil
+	}
+}
+
+// Datastore configures the DHT to use the specified datastore.
+//
+// Defaults to an in-memory (temporary) map.
+func Datastore(ds ds.Batching) Option {
+	return func(o *Options) error {
+		o.Datastore = ds
+		return nil
+	}
+}
+
+// Client configures whether or not the DHT operates in client-only mode.
+//
+// Defaults to false.
+func Client(only bool) Option {
+	return func(o *Options) error {
+		o.Client = only
+		return nil
+	}
+}
+
+// Validator configures the DHT to use the specified validator.
+//
+// Defaults to a namespaced validator that can only validate public keys.
+func Validator(v record.Validator) Option {
+	return func(o *Options) error {
+		o.Validator = v
+		return nil
+	}
+}
+
+// NamespacedValidator adds a validator namespaced under `ns`. This option fails
+// if the DHT is not using a `record.NamespacedValidator` as it's validator (it
+// uses one by default but this can be overridden with the `Validator` option).
+//
+// Example: Given a validator registered as `NamespacedValidator("ipns",
+// myValidator)`, all records with keys starting with `/ipns/` will be validated
+// with `myValidator`.
+func NamespacedValidator(ns string, v record.Validator) Option {
+	return func(o *Options) error {
+		nsval, ok := o.Validator.(record.NamespacedValidator)
+		if !ok {
+			return fmt.Errorf("can only add namespaced validators to a NamespacedValidator")
+		}
+		nsval[ns] = v
+		return nil
+	}
+}
+
+// Protocols sets the protocols for the DHT
+//
+// Defaults to dht.DefaultProtocols
+func Protocols(protocols ...protocol.ID) Option {
+	return func(o *Options) error {
+		o.Protocols = protocols
+		return nil
+	}
+}
