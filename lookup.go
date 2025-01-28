6ae7e8eecd367e5598cac418c831d5ba2eb618ad
a.gorulev <a.gorulev@kernelics.com>
2025-01-28 10:02:30 +0000

    magicwave.io

---
+++ lookup.go
@@ -0,0 +1,116 @@
+package dht
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	cid "github.com/ipfs/go-cid"
+	pb "github.com/libp2p/go-libp2p-kad-dht/pb"
+	kb "github.com/libp2p/go-libp2p-kbucket"
+	peer "github.com/libp2p/go-libp2p-peer"
+	notif "github.com/libp2p/go-libp2p-routing/notifications"
+)
+
+type loggableKey string
+
+func (lk loggableKey) String() string {
+	newKey, err := lk.TryString()
+	if err != nil {
+		log.Debug(err)
+		return string(lk)
+	}
+	return newKey
+}
+
+func (lk loggableKey) TryString() (string, error) {
+	k := string(lk)
+	if len(k) == 0 {
+		return "", fmt.Errorf("loggableKey is empty")
+	}
+	var proto, cstr string
+	if k[0] == '/' {
+		// it's a path (probably)
+		protoEnd := strings.IndexByte(k[1:], '/')
+		if protoEnd < 0 {
+			return k, fmt.Errorf("loggableKey starts with '/' but is not a path: %x", k)
+		}
+		proto = k[1 : protoEnd+1]
+		cstr = k[protoEnd+2:]
+	} else {
+		proto = "provider"
+		cstr = k
+	}
+
+	c, err := cid.Cast([]byte(cstr))
+	if err != nil {
+		return "", fmt.Errorf("loggableKey could not cast key to a CID: %x %v", k, err)
+	}
+	return fmt.Sprintf("/%s/%s", proto, c.String()), nil
+}
+
+// Kademlia 'node lookup' operation. Returns a channel of the K closest peers
+// to the given key
+func (dht *IpfsDHT) GetClosestPeers(ctx context.Context, key string) (<-chan peer.ID, error) {
+	ctx = dht.logSpan(ctx, "GetClosestPeers")
+	log.SetTag(ctx, "key", loggableKey(key))
+
+	tablepeers := dht.routingTable.NearestPeers(kb.ConvertKey(key), AlphaValue)
+	if len(tablepeers) == 0 {
+		log.FinishWithErr(ctx, kb.ErrLookupFailure)
+		return nil, kb.ErrLookupFailure
+	}
+
+	out := make(chan peer.ID, KValue)
+
+	// since the query doesnt actually pass our context down
+	// we have to hack this here. whyrusleeping isnt a huge fan of goprocess
+	parent := ctx
+	query := dht.newQuery(key, func(ctx context.Context, p peer.ID) (*dhtQueryResult, error) {
+		// For DHT query command
+		notif.PublishQueryEvent(parent, &notif.QueryEvent{
+			Type: notif.SendingQuery,
+			ID:   p,
+		})
+
+		pmes, err := dht.findPeerSingle(ctx, p, peer.ID(key))
+		if err != nil {
+			log.Debugf("error getting closer peers: %s", err)
+			return nil, err
+		}
+		peers := pb.PBPeersToPeerInfos(pmes.GetCloserPeers())
+
+		// For DHT query command
+		notif.PublishQueryEvent(parent, &notif.QueryEvent{
+			Type:      notif.PeerResponse,
+			ID:        p,
+			Responses: peers,
+		})
+
+		return &dhtQueryResult{closerPeers: peers}, nil
+	})
+
+	go func() {
+		defer close(out)
+		defer log.Finish(ctx)
+		// run it!
+		res, err := query.Run(ctx, tablepeers)
+		if err != nil {
+			log.SetErr(ctx, err)
+			log.Debugf("closestPeers query run error: %s", err)
+		}
+
+		if res != nil && res.queriedSet != nil {
+			sorted := kb.SortClosestPeers(res.queriedSet.Peers(), kb.ConvertKey(key))
+			if len(sorted) > KValue {
+				sorted = sorted[:KValue]
+			}
+
+			for _, p := range sorted {
+				out <- p
+			}
+		}
+	}()
+
+	return out, nil
+}
